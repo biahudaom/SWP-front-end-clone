331c53a231d058e715766ab7d748fc9afb62f2a1
Hải Anh <93531561+HaiCauMuoi@users.noreply.github.com>
2022-09-21 16:28:28 +0700

    Grid

--- pages/findProject/index.tsx
+++ pages/findProject/index.tsx
@@ -9,7 +9,7 @@ function index() {
     <div className="flex flex-col items-center h-full justify-start">
       <SearchBar />
       <div className="w-full border-t border-t-gray-400 h-full flex justify-center items-center mt-5 pt-10">
-        <ul className="w-[62.5%] h-full grid grid-cols-1 xl:grid-cols-2">
+        <ul className="w-9/12 h-full flex flex-col items-center xl:w-8/12 justify-center xl:grid xl:grid-cols-2 2xl:grid-cols-3 2xl:w-10/12">
           <ProjectCard />
           <ProjectCard />
           <ProjectCard />
